b7e9eba6b708fb83220a2ee5f2ecdcddf264cb5c
Ganeshpatil7722 <ganesh.patilg1111@gmail.com>
2025-06-22 21:44:43 +0530

    Initial frontend commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => <div className="text-center text-2xl">Welcome to File Upload App</div>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
